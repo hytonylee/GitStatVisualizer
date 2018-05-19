91c0d64441a4cab38cc0c8cd38184a0c1a1945b6
Tony Lee <hy.tony.lee@gmail.com>
2018-05-18 22:16:14 -0700

    add App and Index coomponents

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div className="main-app">Hello, World!</div>;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import { Route, BrowserRouter as Router } from "react-router-dom";
+
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
