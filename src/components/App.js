9990aca10bf1abde7d75c849bdbdfc39815770b1
Tony Lee <hy.tony.lee@gmail.com>
2018-06-04 18:52:51 -0700

    edit App Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from "react";
-import Link from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="main-app">
         <header className="main-header">
-          <h1>
-            <Link to="/" />GitHub Public Repo Visualizer
-          </h1>
+          <h1>GitHub Public Repo Visualizer</h1>
         </header>
         <main className="main-content">{this.props.children}</main>
       </div>
