9c80068a913f4bb317e59f3bf2c5366b8975ff66
Tony Lee <hy.tony.lee@gmail.com>
2018-06-06 17:08:23 -0700

    include semantic ui css

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
+import { Grid, Segment } from "semantic-ui-react";
+import 'semantic-ui-css/semantic.min.css';
 
 
 
 class App extends Component {
+  
   render() {
     return (
-      <div className="main-app">
-        <header className="main-header">
-          <h1>
-            React GitHub Project
-          </h1>
-        </header>
-        <main className="main-content">{this.props.children}</main>
-      </div>
+      // <div className="main-app">
+      //   <header className="main-header">
+      //     <h1>
+      //       React GitHub Project
+      //     </h1>
+      //   </header>
+      //   <main className="main-content">{this.props.children}</main>
+      // </div>
+      <Grid columns={3} divided>
+      <Grid.Row stretched>
+        <Grid.Column>
+          <Segment>1</Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>1</Segment>
+          <Segment>2</Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>1</Segment>
+          <Segment>2</Segment>
+          <Segment>3</Segment>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
     );
   }
 }
