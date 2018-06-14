545b5b6f92d2a5ff4992699fca6882fe2ec7c06f
Tony Lee <hy.tony.lee@gmail.com>
2018-06-14 12:51:46 -0700

    update all the change commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,81 @@
-import React, { Component } from "react";
-import { Grid, Segment } from "semantic-ui-react";
-import 'semantic-ui-css/semantic.min.css';
+import React from 'react'
+import axios from 'axios'
 
-
-
-class App extends Component {
-  
-  render() {
-    return (
-      // <div className="main-app">
-      //   <header className="main-header">
-      //     <h1>
-      //       React GitHub Project
-      //     </h1>
-      //   </header>
-      //   <main className="main-content">{this.props.children}</main>
-      // </div>
-      <Grid columns={3} divided>
-      <Grid.Row stretched>
-        <Grid.Column>
-          <Segment>1</Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>1</Segment>
-          <Segment>2</Segment>
-        </Grid.Column>
-        <Grid.Column>
-          <Segment>1</Segment>
-          <Segment>2</Segment>
-          <Segment>3</Segment>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-    );
-  }
+function FormResults(props) {
+  return (
+    <h2>Search results:</h2>
+  )
 }
 
-export { App };
+export class App extends React.Component {
+  
+  constructor (props) {
+    super(props)
+    this.state = {
+      list: [],
+      value: '',
+      result: false
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  
+  handleChange (event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit (event) {
+    event.preventDefault()
+    var username = this.state.value
+    var _this = this
+    this.serverRequest =
+      axios
+        .get('https://api.github.com/users/' + username + '/repos')
+        .then(function (result) {
+          _this.setState({
+            list: result.data,
+            result: 'data-received'
+          })
+        })
+        .catch(function (error) {
+          console.log(error);
+          _this.setState({
+            result: 'error'
+          })
+        });
+    return
+  }
+
+  render () {
+    const hasResult = this.state.result;
+    let response = null;
+    if (hasResult == 'data-received') {
+      response = <h1>User repositories:</h1>;
+    } 
+    if (hasResult == 'error') {
+      response = <h1>No user found</h1>;
+    }
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type='text' ref='term' onChange={this.handleChange}/>
+          </label>
+          <input type='submit' value='Submit'/>
+        </form>
+        <FormResults/>
+        {response}
+        {this.state.list.map(function (repo) {
+            return (
+              <div key={repo.id} className='repo'>
+                <b>{repo.name.toUpperCase()}</b>
+                <p>{repo.description}</p>
+              </div>
+            )  
+          })
+        }
+      </div>
+    )
+  }
+}
